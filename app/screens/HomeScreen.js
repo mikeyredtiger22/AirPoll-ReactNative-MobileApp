3fb2b86e915cf6cd06b86a173ff571766d63f960
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-18 00:10:57 +0000

    Using persistent storage (Expo Secure Store) for key value pairs.

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default class HomePage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { testID: null };
+    this.getSecureStore();
+  }
+
+  setSecureStore = () => {
+    Expo.SecureStore.setItemAsync('testID', 'testValue');
+  };
+
+  getSecureStore = () => {
+    Expo.SecureStore.getItemAsync('testID').then(value => this.setState({ testID: value }));
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text>AirPoll HomePage</Text>
+        <Text>{this.state.testID}</Text>
         <Button title='Start' onPress={() => this.props.navigation.navigate('MapsScreen')}/>
+        <Button title='Set' onPress={this.setSecureStore}/>
+        <Button title='Get' onPress={this.getSecureStore}/>
       </View>
     );
   }
