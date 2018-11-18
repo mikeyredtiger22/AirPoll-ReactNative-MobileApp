bc32f037350aa88e23b9464a9d7ca6114ec1a3f6
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-18 13:07:11 +0000

    Designed and changed splash screen. Added custom button component.

---
+++ app/components/PillButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const PillButton = props => (
+  <TouchableOpacity
+    {...props}
+    style={[styles.button, props.style]}
+    containerStyle={styles.container}
+  >
+    <Text style={styles.text}>{props.text}</Text>
+  </TouchableOpacity>
+);
+
+export default PillButton;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#c7ebcc',
+    paddingHorizontal: 20,
+    borderRadius: 100,
+  },
+  container: {
+    margin: 5,
+  },
+  text: {
+    color: '#005944',
+    fontSize: 35,
+    textAlign: 'center',
+    padding: 8,
+  }
+});
