44ad9390d21f3fa49613084ab841154129f5bdf1
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-18 14:51:51 +0000

    Added sensor input and label with animated views.

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Animated, ImageBackground, StyleSheet, Text, TextInput } from 'react-native';
 import PillButton from '../components/PillButton';
 
 export default class HomePage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { testID: null, showInput: false };
+    this.state = {
+      testID: null,
+      showInput: false,
+      inputText: '',
+      animationPercent: new Animated.Value(0),
+    };
     this.getSecureStore();
   }
+
+  handleStartButton = () => {
+    if (this.state.showInput) {
+      return;
+    }
+    this.setState({ showInput: true });
+    Animated.timing(
+      this.state.animationPercent,
+      {
+        toValue: 100,
+        duration: 500,
+      }
+    ).start();
+  };
 
   setSecureStore = () => {
     Expo.SecureStore.setItemAsync('testID', 'testValue');
@@ -23,10 +42,34 @@ export default class HomePage extends React.Component {
       <ImageBackground
         style={styles.background}
         source={require('../../assets/title-background.png')}>
-        <PillButton
-          text='START'
-          style={styles.startButton}
-          onPress={() => this.props.navigation.navigate('MapsScreen')}/>
+        <Animated.View style={{
+          position: 'absolute',
+          top: this.state.animationPercent.interpolate({
+            inputRange: [0, 100],
+            outputRange: ['70%', '100%'],
+          })
+        }}>
+          <PillButton
+            text='START'
+            style={styles.startButton}
+            onPress={this.handleStartButton}/>
+        </Animated.View>
+        <Animated.View style={{
+          position: 'absolute',
+          width: '60%',
+          top: this.state.animationPercent.interpolate({
+            inputRange: [0, 100],
+            outputRange: ['100%', '50%'],
+          })
+        }}>
+          <Text style={styles.inputLabel}>Enter your sensor ID:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.inputText}
+            keyboardType='numeric'
+          />
+        </Animated.View>
       </ImageBackground>
     );
   }
@@ -40,7 +83,18 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   startButton: {
-    position: 'absolute',
-    top: '70%',
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    backgroundColor: '#d7d7d750',
+    borderRadius: 50,
+    paddingHorizontal: 15,
+    fontSize: 30,
+  },
+  inputLabel: {
+    fontSize: 20,
+    alignSelf: 'center',
+    marginBottom: 5,
   }
 });
