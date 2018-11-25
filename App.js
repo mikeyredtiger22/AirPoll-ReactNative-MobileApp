c9434cffe25bccec7e0fe8841a5915768f895afa
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-25 16:50:42 +0000

    Added map and profile screen tab navigator.

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createAppContainer, createBottomTabNavigator, createSwitchNavigator } from "react-navigation";
+import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from './app/screens/HomeScreen';
 import MapsScreen from './app/screens/MapsScreen';
+import ProfileScreen from './app/screens/ProfileScreen';
+
+const MapsTabNavigator = createBottomTabNavigator({
+  MapsScreen,
+  ProfileScreen
+}, {
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ tintColor }) => {
+      const { routeName } = navigation.state;
+      const maps = (routeName === 'MapsScreen');
+      const iconName = maps ? 'md-compass' : 'md-options';
+      return <Ionicons name={iconName} size={25} color={tintColor} />;
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: '#03a6b0',
+    inactiveTintColor: '#2b2b2b',
+  },
+});
 
 const AppNavigator = createSwitchNavigator({
   HomeScreen,
-  MapsScreen
+  MapsTabNavigator,
 }, {initialRouteName: 'HomeScreen'});
 
 const AppContainer = createAppContainer(AppNavigator);
